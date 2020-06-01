acb47b1903d88e4638de49ce627b5def4877785d
SITRUK <kurtis.santillanes@gmail.com>
2020-05-31 23:25:32 -0700

    bug fix in api/profile. cleared db users in test db just in cases.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,9 +7,9 @@ const { check, validationResult } = require('express-validator');
 const normalize = require('normalize-url');
 const checkObjectId = require('../../middleware/checkObjectId');
 
-const Post = require('../../models/Post');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route   GET api/profile/me
 // @desc    Get current user profile
